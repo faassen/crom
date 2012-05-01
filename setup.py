4616a2c832d875295af93ade2a1cb4ac6bc92759
faassen <faassen@startifact.com>
2012-05-01 17:12:53 +0200

    Basic application structure.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name='crom',
+      version = '0.1dev',
+      description="Components.",
+      author="Martijn Faassen",
+      author_email="faassen@startifact.com",
+      license="BSD",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'grokker'],
+      extras_require = dict(
+        test=['pytest >= 2.0'],
+        ),
+      entry_points="""
+      # Add entry points here
+      """,
+      )
