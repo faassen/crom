69f73adbd03945d5735a529e3a7fd8bb4e768d6a
faassen <faassen@startifact.com>
2012-05-01 19:27:22 +0200

    More tests.

--- crom/test_component.py
+++ crom/test_component.py
@@ -48,6 +48,29 @@ def test_instance_class_based_registration():
 
     alpha = Alpha()
     assert reg.lookup([alpha], ITarget, '') is foo
+
+def test_instance_inheritance():
+    reg = InstanceRegistry()
+    foo = object()
+
+    class Gamma(object):
+        pass
+
+    class Delta(Gamma):
+        pass
+    
+    reg.register([Gamma], ITarget, '', foo)
+
+    delta = Delta()
+    
+    assert reg.lookup([delta], ITarget, '') is foo
+
+def test_instance_not_found():
+    reg = InstanceRegistry()
+    
+    assert reg.lookup([], ITarget, '') is None
+    alpha = Alpha()
+    assert reg.lookup([alpha], ITarget, '') is None
     
 def test_utility_no_source():
     reg = Registry()
