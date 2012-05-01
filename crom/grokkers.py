9cc9f26a296db60b509361ce1268662c87a02e00
faassen <faassen@startifact.com>
2012-05-01 23:49:41 +0200

    Use component for registration, not utility.

--- crom/grokkers.py
+++ crom/grokkers.py
@@ -6,7 +6,7 @@ from .directives import source, target, name, registry
 @directive(target)
 @directive(name)
 @directive(registry)
-def utility(scanner, pyname, obj, source, target, name, registry):
+def component(scanner, pyname, obj, source, target, name, registry):
     pass
 
 @grokker
